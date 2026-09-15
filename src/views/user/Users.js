1f5b443b91bc6455386357d77c96a6754bbc658d
Frisk13108 <pietrof3108@gmail.com>
2026-09-15 17:09:40 -0300

    FIX: Desc da Kátia arrumada

--- src/views/user/Users.js
+++ src/views/user/Users.js
@@ -338,7 +338,7 @@ export const users = [
         nome: 'Kátia',
         pfp: 'https://i1.rgstatic.net/ii/profile.image/1062860247531523-1630417038409_Q512/Katia-Siewert.jpg',
         banner: 'https://tse4.mm.bing.net/th/id/OIP.aBDOOuiC3X-w3RTMNH09-AHaEK?r=0&rs=1&pid=ImgDetMain&o=7&rm=3',
-        desc: 'Doutorado em Educação pelo PPGE da Universidade do Estado de Santa Catarina (UDESC). Mestrado em Computação Aplicada pela Universidade do Vale do Rio dos Sinos (UNISINOS). Bacharel em Arquitetura e Urbanismo e Licenciatura em Matemática pela Universidade Regional de Blumenau (FURB). Integrante do grupo de pesquisa NEPesTEEM',
+        desc: 'Doutorado em Educação pelo PPGE da UDESC. Mestrado em Computação Aplicada pela UNISINOS. Bacharel em Arquitetura e Urbanismo e Licenciatura em Matemática pela FURB. Integrante do grupo de pesquisa NEPesTEEM',
         mostrarSala: 'sim',
         seguidores: 32,
         seguindo: 9,
